6a5922a33bc58b0090e132f38308859cbbc90c05
KinKinoV <mr.kolbasa.ask@gmail.com>
2024-05-29 15:04:29 +0300

    [skip_tests] Changed default values for testing environment

--- infrastructure/00-params.tf
+++ infrastructure/00-params.tf
@@ -3,43 +3,43 @@
 #### VPC Network
 variable "vpc_cidr" {
   type    = string
-  default = "192.168.0.0/16"
+  default = "100.100.0.0/16"
 }
 
 #### HTTP PARAMS
 variable "network_http" {
   type = map(string)
   default = {
-    subnet_name = "subnet_http"
-    cidr        = "192.168.1.0/24"
+    subnet_name = "testing-subnet_http"
+    cidr        = "100.100.1.0/24"
   }
 }
 
 # Set number of instance
 variable "http_instance_names" {
   type    = set(string)
-  default = ["instance-http-1", "instance-http-2"]
+  default = ["testing-instance-http-1", "testing-instance-http-2"]
 }
 
 #### DB PARAMS
 variable "network_db" {
   type = map(string)
   default = {
-    subnet_name = "subnet_db"
-    cidr        = "192.168.2.0/24"
+    subnet_name = "testing-subnet_db"
+    cidr        = "100.100.2.0/24"
   }
 }
 
 # Set number of instance
 variable "db_instance_names" {
   type    = set(string)
-  default = ["instance-db-1", "instance-db-2", "instance-db-3"]
+  default = ["testing-instance-db-1", "testing-instance-db-2", "testing-instance-db-3"]
 }
 
 variable "public_key_name" {
   type        = string
   description = "Name of the public key to use"
-  default     = "test-key"
+  default     = "testing-test-key"
 }
 
 # Set number of instance
